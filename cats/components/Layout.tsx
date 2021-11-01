f803f1682fabf7c4a0297ceefd02d6d0467cff3d
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-02 01:10:41 +0200

    Created Main Layout

---
+++ cats/components/Layout.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+function Layout({ children, title }: any) {
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+			}}
+		>
+			<Head>
+				<title>{title}</title>
+			</Head>
+			<Navbar />
+			<main>{children}</main>
+		</div>
+	);
+}
+
+export default Layout;
