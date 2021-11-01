0fbd6c468da0e112ec6ec3b48bf2fdc3459be51c
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-02 01:21:41 +0200

    Added FavouritesPage

---
+++ cats/pages/favourites.tsx
@@ -0,0 +1,9 @@
+import Layout from "@components/Layout";
+import { NextPage } from "next";
+import React from "react";
+
+const favourites: NextPage = () => {
+	return <Layout title="Favourites"></Layout>;
+};
+
+export default favourites;
