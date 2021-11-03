f89b9062d1840a8e506f757813eb2fdda711d41d
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-03 03:03:20 +0200

    Added OnClick event emitter.

--- cats/components/Heart/index.tsx
+++ cats/components/Heart/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Heart.module.css";
-function index() {
-	return <div className={styles.heart} />;
+function index({ onClick }: { onClick: Function }) {
+	return <div onClick={() => onClick()} className={styles.heart} />;
 }
 
 export default index;
