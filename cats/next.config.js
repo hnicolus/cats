7522a5cfe630e4bad23bc29a0fa1dcad3ef93787
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-03 03:36:44 +0200

    Added BASE Endpoint again.

--- cats/next.config.js
+++ cats/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	reactStrictMode: true,
 	env: {
 		CAT_API_TOKEN: "",
-		BASE_API_ENDPOINT: "",
+		BASE_API_ENDPOINT: "https://api.thecatapi.com/v1/",
 	},
 	images: {
 		domains: ["cdn2.thecatapi.com"],
