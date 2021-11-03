744902197555dc8f689227d0650a7780fee164fc
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-03 03:08:10 +0200

    Added Favourites Context andToastify Container

--- cats/pages/_app.tsx
+++ cats/pages/_app.tsx
@@ -1,16 +1,56 @@
+import "react-toastify/dist/ReactToastify.css";
 import "nprogress/nprogress.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NProgress from "nprogress";
 import Router from "next/router";
+import FavouriteContext, { IFavouriteContext } from "context/favouritesContext";
+import {
+	addToFavourites,
+	getFavourites,
+	removeFromFavourites,
+} from "@services/catsService";
 
+import { ToastContainer } from "react-toastify";
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const [favouriteContext, setFavourite] = useState<IFavouriteContext>({
+		items: [],
+		onAdded: (cat: any) => {
+			(async () => {
+				await addToFavourites(cat);
+				loadFavourites();
+			})();
+		},
+		onRemoved: (catId: any) => {
+			(async () => {
+				await removeFromFavourites(catId);
+				loadFavourites();
+			})();
+		},
+	});
+	const loadFavourites = () => {
+		(async () => {
+			const tmpFav = { ...favouriteContext };
+			tmpFav.items = await getFavourites();
+			setFavourite(tmpFav);
+		})();
+	};
+	useEffect(() => {
+		loadFavourites();
+	}, []);
+	return (
+		<>
+			<FavouriteContext.Provider value={favouriteContext}>
+				<Component {...pageProps} />
+				<ToastContainer />
+			</FavouriteContext.Provider>
+		</>
+	);
 }
 
 export default MyApp;
